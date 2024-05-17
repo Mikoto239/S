01c03b6b8a4bff6cc74936266af8942dd2aa0f5b
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-18 04:47:29 +0800

    Update adminReport.jsx

--- client/src/pages/admin/adminReport.jsx
+++ client/src/pages/admin/adminReport.jsx
@@ -33,7 +33,7 @@ const AdminReport = () => {
 
   const handleDate = async (month, year) => {
     try {
-      const getme = await axios.get('/api/getme', {
+      const getme = await axios.get('https://cotmemo.onrender.com/api/getme', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -41,7 +41,7 @@ const AdminReport = () => {
 
       const email = getme.data.user.email;
 
-      const Allreport = await axios.post('/api/allreport', {
+      const Allreport = await axios.post('https://cotmemo.onrender.com/api/allreport', {
         email,
         month,
         year,
